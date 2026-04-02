9b4fed05b5f9e5522e259b4015d9841da9c46486
Shivam5298 <sganguly2096@gmail.com>
2026-04-02 20:24:01 +0530

    Version 1.0

---
+++ Mathematical Expressions.py
@@ -0,0 +1,56 @@
+print("This is a Sample Program to go through Simple Mathematical Operations")
+
+num1 = 10
+num2 = 2
+
+addition = num1 + num2 # Addition
+print(num1,"+",num2,"=",addition)
+
+subtraction = num1 - num2 # subtraction
+print(num1,"-",num2,"=",subtraction)
+
+multiplication = num1*num2
+print(num1,"*",num2,"=",multiplication)
+
+division = num1/num2
+print(num1,"/",num2,"=",division)
+
+exponent = num1 ** num2
+print(num1,"**",num2,"=",exponent)
+
+quotient = num1 // num2
+print(num1,"//",num2,"=",quotient)
+
+remainder = num1 % num2
+print(num1,"%",num2,"=",remainder)
+
+
+num3 = 10
+print("lets now understand assignment operators and each iteration starts with num3 = 10")
+
+num3 +=5
+print("New value is:",num3,"(after addition reaasignment by 5)")
+
+num3 = 10
+num3 -=5
+print("New value is:",num3,"(after subtraction reassignment by 5)")
+
+num3 = 10
+num3 *=5
+print("New value is:",num3,"(after multiplication reassignment by 5)")
+
+num3 = 10
+num3 /=2
+print("New value is:",num3,"(after division reassignment by 2)")
+
+num3 = 10
+num3 **=2
+print("New value is:",num3,"(after exponential reassignment by 2)")
+
+num3 = 10
+num3 //= 2
+print("New value is:",num3,"(after quotient reassignment by 2)")
+
+num3 = 10
+num3 %=2
+print("New value is:",num3,"(after modulus reassignment by 2)")
--- variables.py
+++ variables.py
@@ -1,7 +1,7 @@
 print("This is a variables example")
-num1 = 12
-float2 = 1.365
-bool1 = True
+num1 = 12 # Integers
+float2 = 1.365 # Float
+bool1 = True # Boolean
 print(num1)
-num1 = 21
+num1 = 21 # Reassignment
 print("Reassigned value =",num1)
