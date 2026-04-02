eb241768a926d7e435ac1c3e9ffec5c633692ec6
Shivam5298 <sganguly2096@gmail.com>
2026-04-02 19:16:19 +0530

    Version 1.0

---
+++ sample.py
@@ -0,0 +1,4 @@
+print("hello world")
+ex_var = 12
+ex_var2 = 12
+print(ex_var + ex_var2)
---
+++ variables.py
@@ -0,0 +1,7 @@
+print("This is a variables example")
+num1 = 12
+float2 = 1.365
+bool1 = True
+print(num1)
+num1 = 21
+print("Reassigned value =",num1)
