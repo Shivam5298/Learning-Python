c06589bc8628dd8bb60bd1f789adc2a08e0c9ce3
Shivam5298 <sganguly2096@gmail.com>
2026-04-06 13:22:49 +0530

    Version 1.0

---
+++ Functions.py
@@ -0,0 +1,12 @@
+# A sample program demonstrating the use of functions in Python
+
+def hello():
+    print("Hello, World!")
+
+hello()
+
+def greet():
+    name = input("Enter your name: ")
+    print("Hello, " + name + "!")
+
+greet()
