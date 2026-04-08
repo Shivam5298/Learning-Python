be7832bc6b021ff85ba3bbf8162a63724e067b86
Shivam5298 <sganguly2096@gmail.com>
2026-04-08 19:45:34 +0530

    Version 1.0

---
+++ FizzBuzz Exercise.py
@@ -0,0 +1,14 @@
+num = 1
+
+while num <=50:
+    if num % 3 ==0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
+    num +=1
+
+
