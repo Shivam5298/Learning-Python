c3e5ce5d483d36745465615829152754c1c2c961
Shivam5298 <sganguly2096@gmail.com>
2026-04-10 18:46:14 +0530

    Version 1.0

---
+++ List Exercise2.py
@@ -0,0 +1,17 @@
+example1 = [[0,2],[4,6],[8,10],[12,14]]
+
+print(example1[0])
+
+print(example1[3][1])
+
+example2 = ["chair","table","desk","lamp","bed"]
+
+print(example2[-5])
+
+print("Most people own at least",example1[0][1],example2[0],".")
+
+example3 = [0.98, 8.76, 6.54, 4.320]
+
+print(example3[1:])
+print(example3[1:3])
+print(example3[:2])
