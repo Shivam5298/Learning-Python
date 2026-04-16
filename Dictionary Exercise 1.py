bb8382337182987466de808f099ce411d57382aa
Shivam5298 <sganguly2096@gmail.com>
2026-04-16 13:33:34 +0530

    Version 1.0

---
+++ Dictionary Exercise 1.py
@@ -0,0 +1,15 @@
+# This is a dictionary exercise.
+
+person = {
+    "firstName": "Shivam",
+    "lastName": "Ganguly",
+    "age": 29,
+    "email": "sganguly2096@gmail.com",
+    "isActive": True
+}
+
+print("Email = ",person["email"])
+
+print("item" in person)
+
+print("item" not in person)
