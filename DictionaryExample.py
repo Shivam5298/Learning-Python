fca224c11083f2770f27a3d36d1d408e4bb14583
Shivam5298 <sganguly2096@gmail.com>
2026-04-16 13:08:40 +0530

    Version 1.0

--- DictionaryExample.py
+++ DictionaryExample.py
@@ -5,13 +5,15 @@ person = {
     "name": "Shivam Ganguly",
     "age": 29,
     "city": "kolkata",
-    "email": "sganguly2096@gmail.com"
+    "email": "sganguly2096@gmail.com",
+    "UUID": "123e4567-e89b-12d3-a456-426614174000"
 
 }
 
 # nested dictionary
 personDatabase = [
     {
+        "UUID": "123e4567-e89b-12d3-a456-426614174000",
         "name": "Shivam Ganguly",
         "age": 29,
         "city": "kolkata",
@@ -24,6 +26,7 @@ personDatabase = [
         "email": "sganguly2096@gmail.com"
     },
     {
+        "UUID": "123e4567-e89b-12d3-a456-426614174001",
         "name": "John Doe",
         "age": 30,
         "city": "New York",
@@ -36,3 +39,22 @@ personDatabase = [
         "email": "johndoe@example.com"
     }
 ]
+
+print(person["name"])  # Output: Shivam Ganguly
+print(person["age"])   # Output: 29
+print("Details of the first person in the database:" )
+print(personDatabase[0])  # Output: {'name': 'Shivam Ganguly', 'age': 29, 'city': 'kolkata', 'fullAddress': {'street': '123 Main St', 'state': 'WB', 'zip': 700032, 'country': 'India'}, 'email': 'sganguly2096@gmail.com'} 
+
+
+# Anoteher way to access the values in a dictionary is to use the get() method, which returns None if the key is not found instead of raising an error
+print("Database details of Shivam in the database:" )
+
+for item in personDatabase:
+    if item["name"] == "Shivam Ganguly":
+        UUID = item["UUID"]
+
+for item in personDatabase:
+    if item["UUID"] == UUID:
+        details = item
+
+print(details)  # Output: {'UUID': '123e4567-e89b-12d3-a456-426614174000', 'name': 'Shivam Ganguly', 'age': 29, 'city': 'kolkata', 'fullAddress': {'street': '123 Main St', 'state': 'WB', 'zip': 700032, 'country': 'India'}, 'email': '
