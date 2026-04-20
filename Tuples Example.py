6c3475aeac3ae5a65047c2f3d45a325690919f44
Shivam5298 <sganguly2096@gmail.com>
2026-04-20 11:11:44 +0530

    Version 1.0

---
+++ Tuples Example.py
@@ -0,0 +1,12 @@
+tup1 = ("a", "b", "c", "d", "e") # can be of same data type
+tup2 = (1.123, True, [1, 2, 3], {"abc": "xyz", "123": "456"}) # can be a mix of different data types
+tup3 = (1, 2, 3 , 4, 5, 5, 5) # can have same element multiple times
+
+tup4 = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+print(tup4[2])
+print(tup4[:8])
+print(tup4[2:7])
+print(tup4[3:])
+
+ 
