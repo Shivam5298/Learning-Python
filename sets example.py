d4e08ff91f9ac1ee218ab8a80bd6f79a3c8a9546
Shivam5298 <sganguly2096@gmail.com>
2026-04-29 13:29:10 +0530

    Version 1.0

--- sets example.py
+++ sets example.py
@@ -1,2 +1,10 @@
+# Sets contain unique items and they ignore the occurence of any duplicate items.
+# Sets are unordered hence the items dont have a specific order
+
 set1 = {1, 2, 3, 4}
 set2 = set({"a", "b", "c", "d", "e"})
+
+print(set1)
+print(set2)
+
+# print(set1[2]) // cant be used with indexes
